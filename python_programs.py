d6aaf31d88220a058646d40c1bf24b37f3fa9e55
Prasadaketi <104889674+Prasadaketi@users.noreply.github.com>
2023-11-01 16:02:15 +0530

    Add files via upload

---
+++ python_programs.py
@@ -0,0 +1,45 @@
+#arthmetic calculations using functions
+#addition
+def add(num1, num2):
+    return num1 + num2
+num1 : int = 10
+num2 = int(15)
+print(add(num1, num2))
+
+#subraction
+def sub(num1, num2):
+    return num1 - num2
+num1 : int = 30
+num2 : int = 15
+print(sub(num1, num2))
+
+#multiplication
+
+def mul(num1, num2):
+    return num1 * num2
+num1 = int(10)
+num2 = int(5)
+print(mul(num1, num2))
+
+#division
+
+def div(num1, num2):
+    return num1 / num2
+num1 = int(25)
+num2 = int(5)
+print(div(num1, num2))
+
+#modulo division
+
+def module(num1, num2):
+    return num1 % num2
+num1 = int(25)
+num2 = int(4)
+print(module(num1, num2))
+
+#floor division
+def fldiv(num1, num2):
+    return num1 // num2
+num1 = int(25)
+num2 = int(4)
+print(fldiv(num1, num2))
